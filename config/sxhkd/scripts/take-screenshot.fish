2d392e3b9bfe2e6a67033d8b05c0765d81119a10
ayanwx <ayanxdev@gmail.com>
2024-05-15 15:13:53 +0000

    ..

--- config/sxhkd/scripts/take-screenshot.fish
+++ config/sxhkd/scripts/take-screenshot.fish
@@ -2,4 +2,7 @@ set dir "$HOME/Pictures/Screenshots"
 set filename (date +'%d%m%Y-%H%M%S')
 
 maim -f png -su $dir/$filename.png
-cat $dir/$filename.png | xclip -selection clipboard -t image/png && notify-send "Screenshot captured and copied to clipboard"
+
+cat $dir/$filename.png \
+    | xclip -selection clipboard -t image/png \
+    && notify-send "$filename.png" --icon $dir/$filename.png
